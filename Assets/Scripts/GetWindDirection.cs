eec6bad15d20f94a7c704164e67fd131a26a4998
lyp3238 <lyp3238@gmail.com>
2020-10-18 03:53:30 +0900

    어느정도 완성된 예제 업로드

---
+++ Assets/Scripts/GetWindDirection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetWindDirection : MonoBehaviour
+{
+    public Transform windDirection;
+    public float windPower = 5f;
+    private Vector3 setDirection;
+    private Cloth myCloth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCloth = GetComponent<Cloth>();
+        setDirection = windDirection.forward;
+        myCloth.externalAcceleration = setDirection * windPower;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PushWind(Transform originPoint, float windPower)
+    {
+        Vector3 windDirection = (transform.position - originPoint.position).normalized;
+        myCloth.externalAcceleration = windDirection * 25f;
+        StopCoroutine("SetWorldWind");
+        StartCoroutine("SetWorldWind", 1f);
+    }
+    IEnumerator SetWorldWind(int timer)
+    {
+        yield return new WaitForSeconds(timer);
+        Debug.Log("Hello");
+
+        myCloth.externalAcceleration = setDirection * this.windPower;
+    }
+}
---
+++ Assets/Scripts/Movemnet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movemnet : MonoBehaviour
+{
+    public float speed = 10f;
+    public float rotationSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
